f2fa3c884e32882522165e97d0f2efff2ecf178d
Mahedi <mahedimridul57@gmail.com>
2024-12-07 19:19:06 +0100

    added http response helper

---
+++ DrinkApp/Users/Model/DTO/RespononseDTO/HttpResponseHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
+public static class HttpResponseHelper
+{
+    public static HttpResponseData CreateResponseData(HttpRequestData req, HttpStatusCode statusCode, object content)
+    {
+        var response = req.CreateResponse();
+        if (content != null)
+        {
+            response.WriteAsJsonAsync(content);
+        }
+        response.StatusCode = statusCode;
+        return response;
+    }
+}
